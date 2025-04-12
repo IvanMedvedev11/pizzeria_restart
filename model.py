d1116821e8b42363c87b7b297e1890cc4b374053
Median72 <medvivan11@gmail.com>
2025-04-12 18:07:54 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,38 +1,54 @@
-import json
 import hashlib
+import sqlite3
 class FileManager:
-    def save_to_json(self, filename, data):
-        with open(filename, 'w') as file:
-            json.dump(data, file)
-    def save_to_txt(self, filename, data):
-        with open(filename, 'w') as file:
-            file.write(data)
-    def load_json(self, filename):
-        with open(filename, 'r') as file:
-            json.load(file)
-    def load_txt(self, filename):
-        with open(filename, 'r') as file:
-            file.read()
+    def __init__(self):
+        self.connection = sqlite3.connect("database.db")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXIST Users (
+        first_name TEXT IS NOT NULL
+        last_name TEXT IS NOT NULL
+        birthday DATETIME
+        number TEXT IS NOT NULL
+        )''')
+        self.connection.commit()
+        self.cursor.execute('''CREATE TABLE IF NOT EXIST Products (
+                name TEXT IS NOT NULL
+                price INTEGER
+                count INTEGER
+                )''')
+        self.connection.commit()
+        self.cursor.execute('''CREATE TABLE IF NOT EXIST Products_18+ (
+                        name TEXT IS NOT NULL
+                        price INTEGER
+                        count INTEGER
+                        )''')
+        self.connection.commit()
+class User:
+    def __init__(self):
+        self.databaser = FileManager()
+    def add_user(self, first_name, last_name, birthday, number):
+        self.databaser.cursor.execute('''SELECT * FROM Users WHERE number = ?''', (number))
+        user = self.databaser.cursor.fetchone()
+        if user is not None:
+            return "Пользователь уже существует"
+        self.databaser.cursor.execute('''INSERT INTO Users (first_name, last_name, birthday, number) VALUES (?, ?, ?, ?)''', (first_name, last_name, birthday, number))
+        self.databaser.connection.commit()
+        return "Вы успешно зарегистрированы"
 class Pizza:
-    def __init__(self, name, database):
+    def __init__(self, name):
+        self.databaser = FileManager()
         self.name = name
-        self.database = database
         self.price = 0
-    def add_ingredient(self, ingredient, count):
-        for elem in self.database:
-            if elem['name'] == ingredient:
-                if elem['count'] < count:
-                    return "Не хватает ингредиентов"
-                else:
-                    elem['count'] -= count
-                    index = self.database.index(elem)
-                    self.database.pop(index)
-                    self.database.insert(elem, index)
-                ingredient_info = elem
-                break
-        else:
-            return "Ингредиент не найден"
-        self.price += ingredient_info['price'] * count
+    def add_ingredient(self, ingredient, count, table_name):
+        self.databaser.cursor.execute('''SELECT count FROM ? WHERE name = ?''', (table_name, ingredient))
+        product_count = self.databaser.cursor.fetchone()[0]
+        if count > product_count:
+            return "Не хватает ингредиентов"
+        self.databaser.cursor.execute('''UPDATE ? SET count = count - ? WHERE name = ?''', (table_name, count, ingredient))
+        self.databaser.connection.commit()
+        self.databaser.cursor.execute('''SELECT price FROM ? WHERE name = ?''', (table_name, ingredient))
+        price = self.databaser.cursor.fetchone()[0]
+        self.price += price * count
     def add_size(self, size):
         size_coefficients = {"low": 0.8, "medium": 1, "high": 1.2}
         self.price *= size_coefficients[size]
