7b5c41d1c72dee1aaad74734b0ede7d099b14e91
Anzam2000 <andrejzamaraev29@gmail.com>
2025-04-18 08:15:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 
+from tkinter import Tk
+from view import Authorization_Root
+from controller import PizzaController
+
+
+def main():
+    root = Tk()
+
+    view = Authorization_Root(root)
+
+    controller = PizzaController(view)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
